c1216e71d0df12dbdac45f1f65da0ff396d2f6c4
DaeHyeonHong <hongdae@zen9ss.co.kr>
2021-06-08 22:49:15 +0900

    TypeScript
    getter, setter

--- typescript/3-OOP/3-3-encapsulation.ts
+++ typescript/3-OOP/3-3-encapsulation.ts
@@ -44,4 +44,29 @@
 
     const maker = CoffeeMaker.makeMachine(32);
 
+    class User {
+
+        get fullName(): string {
+            return `${this.firstName} ${this.lastName}`
+        }
+
+        private internalAge = 4;
+
+        get age(): number {
+            return this.internalAge;
+        }
+
+        set age(num: number) {
+            if (num > 0)
+                this.internalAge = num;
+        }
+
+        constructor(private firstName: string, private lastName: string) {
+        }
+    }
+
+    const user = new User('Steve', 'Jobs');
+    user.age = 6;
+    console.log(user.fullName);
+
 }
